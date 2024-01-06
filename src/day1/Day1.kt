9538fb2e3d297099cf8060bbfe878daf779b0c7b
Alex Clapa <alexandru.clapa@gmail.com>
2024-01-06 18:56:16 +0000

    Day2 and Day1 adjustments

--- src/day1/Day1.kt
+++ src/day1/Day1.kt
@@ -2,16 +2,16 @@ package day1
 
 import java.io.File
 
-var input: List<String> = emptyList()
+var input: MutableList<String> = emptyList<String>().toMutableList()
 
 fun read() {
     File("src/day1/input.txt").forEachLine { input += it }
 }
 
 fun solve() {
-    input = input.map { it.replace("[^0-9]".toRegex(), "") }
+    val modifiedList = input.map { it.replace("[^0-9]".toRegex(), "") }
     var sum = 0
-    input.forEach {
+    modifiedList.forEach {
         sum = if (it.length > 1) sum + (it[0].digitToInt() * 10 + it[it.length-1].digitToInt()) else sum + (it.toInt() * 10 + it.toInt())
     }
     println(sum)
---
+++ src/day2/Day2.kt
@@ -0,0 +1,47 @@
+package day2
+
+import java.io.File
+
+fun main() {
+    val allowedValues = mapOf(
+            "red" to 12,
+            "green" to 13,
+            "blue" to 14
+            )
+
+    var counter = 0
+    var possible = true
+    var sum = 0
+    var powerSum = 0
+
+    File("src/day2/input.txt").forEachLine {
+        counter += 1
+        val groupMinimums = mutableMapOf(
+                "red" to 0,
+                "green" to 0,
+                "blue" to 0
+        )
+
+        val groups = "(\\d+) (\\w+)".toRegex().findAll(it, 0)
+        for (group in groups) {
+            val value = group.groupValues[1].toInt()
+            val colour = group.groupValues[2]
+
+            if (value > allowedValues[colour]!!) {
+                possible = false
+            }
+
+            if (value > groupMinimums[colour]!!) {
+                groupMinimums[colour] = value
+            }
+        }
+        if (possible) {
+            sum += counter
+        }
+        possible = true
+
+        powerSum += groupMinimums["red"]!! * groupMinimums["green"]!! * groupMinimums["blue"]!!
+    }
+    println(sum)
+    println(powerSum)
+}
